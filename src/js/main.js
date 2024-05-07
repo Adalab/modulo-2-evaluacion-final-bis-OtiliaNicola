3ca94f2e00e4f406f450bdc4d875ac661075d23a
OtiliaNicola <julio.guerrero@ezentis.com>
2024-05-08 00:55:29 +0200

    Second commit JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('>> Ready :)');
+//variables globales
+const saveBtn = document.querySelector('.js_btnSave');
+const recoverBtn = document.querySelector('.js_btnRecover');
+const ul = document.querySelector('.js_ul');
+
+//Array vacío
+let userlist = [];
+
+
+
+//Función en la que me pinta los 10 usuarios
+function init ()
+    const url = `https://randomuser.me/api/?results=10`;
+    //llamanos a la API
+    fetch(url)
+    .then(response => response.json())
+    .then(data => set)
