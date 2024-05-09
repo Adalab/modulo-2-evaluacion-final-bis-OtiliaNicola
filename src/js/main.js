24ebde8046b91a3b088b1fb217edb1423c33a56b
OtiliaNicola <julio.guerrero@ezentis.com>
2024-05-10 01:20:46 +0200

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,45 @@
 'use strict';
 
 //variables globales
-const saveBtn = document.querySelector('.js_btnSave');
-const recoverBtn = document.querySelector('.js_btnRecover');
-const ul = document.querySelector('.js_ul');
+const ulList = document.querySelector('.js_users');
 
-//Array vacío
-let userlist = [];
+let usersData = []; //array vacío
+let usersFriends = [];//array isFriend
+
+const url = "https://randomuser.me/api/?results=10";
+
+//Funciones
+
+//10 usuarios al azar
+function renderRandom(user) {
+    console.log(user);
+    return `<li  id="${user.id.value}" class="user-li js_list">
+        <img src="${user.picture.large}"/>
+        <h3> ${user.name.first} ${user.name.last}</h3>
+        <p>${user.location.city}</p>
+        <p>${user.login.username}</p>
+    </li>`;
+}
 
 
+function renderFriends(){
+    
+    const allUserLi = document.querySelectorAll('.js_list');
+    for (const li of allUserLi) {
+        li.addEventListener('click', addFriend);
+    }
+}
 
-//Función en la que me pinta los 10 usuarios
-function init ()
-    const url = `https://randomuser.me/api/?results=10`;
-    //llamanos a la API
+const getData = () => {
     fetch(url)
-    .then(response => response.json())
-    .then(data => set)
+    .then(response=> response.json())
+    .then((dataApi)=>{
+        usersData = dataApi.results;
+        usersData.forEach(user => {
+            ulList.innerHTML += renderRandom(user);
+        });
+    localStorage.setItem("userData", JSON.stringify(usersData));
+    });    
+};
+//se ejecuta cuando carga la págima
+getData();
