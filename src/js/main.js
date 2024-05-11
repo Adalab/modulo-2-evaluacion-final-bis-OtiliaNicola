d70e3effc5ef2fec84e5e9ecf87e5bf35506164c
OtiliaNicola <julio.guerrero@ezentis.com>
2024-05-11 18:03:34 +0200

    3.Save in LS

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,12 @@
 //variables globales
 const ulList = document.querySelector('.js_users');
 const saveBtn = document.querySelector('.js_btnSave');
-const recoverBtn = document.querySelector('js_btnRecover');
+const recoverBtn = document.querySelector('.js_btnRecover');
 let usersData = []; //array vacío
 const url = "https://randomuser.me/api/?results=10";
-
 //Funciones
 function renderRandom(user) {//10 usuarios al azar
-    return `<li  id="${user.id.value}" class="user-li js_list">
+    return `<li  id="${user.login.uuid}" class="user-li js_list">
         <img src="${user.picture.large}"/>
         <h3> ${user.name.first} ${user.name.last}</h3>
         <p>${user.location.city}</p>
@@ -18,7 +17,7 @@ function renderRandom(user) {//10 usuarios al azar
 
 function addFriend(ev) {
     const liClikedId = ev.currentTarget.id; //obtener todos los datos del usuario
-    const clikedUsersData = usersData.findIndex((item) => item.id.value === liClikedId);
+    const clikedUsersData = usersData.findIndex((item) => item.login.uuid === liClikedId);
     usersData[clikedUsersData].isFriend = true;
     renderUsers(); //vuelvo a renderizar la lista nuevamente
 }
@@ -27,7 +26,7 @@ function renderUsers() {//función para renderizar la lista de usuarios
     ulList.innerHTML = '';//limpiar el contenid actual de la li
     usersData.forEach(user => {
         const backgroundColor = user.isFriend ? 'pink' : '#a3e2f5';
-        const usersHTML = `<li  id="${user.id.value}" class="user-li js_list" style="background-color: ${backgroundColor}">
+        const usersHTML = `<li  id="${user.login.uuid}" class="user-li js_list" style="background-color: ${backgroundColor}">
         <img src="${user.picture.large}"/>
         <h3> ${user.name.first} ${user.name.last}</h3>
         <p>${user.location.city}</p>
@@ -50,18 +49,16 @@ const getData = () => {
         .then((dataApi) => {
             usersData = dataApi.results;
             renderUsers();
-            localStorage.setItem("userData", JSON.stringify(usersData));
+            //localStorage.setItem("userStoredList", JSON.stringify(usersData));
         });
 };
-
 function handleSave(event){
     event.preventDefault();
-    localStorage.setItem("userData", JSON.stringify(usersData));
+    localStorage.setItem("userStoredList", JSON.stringify(usersData));
 }
-
 function handleRecover(event){
     event.preventDefault();
-    const savedUsers = JSON.parse(localStorage.getItem(usersData)) || [];
+    const savedUsers = JSON.parse(localStorage.getItem("userStoredList"));
     usersData=savedUsers;
     renderUsers();
 }
